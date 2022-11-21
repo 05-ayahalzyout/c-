9f4e10dafc1b920fc04e8d4bb67768646c24953a
05-ayahalzyout <115452706+05-ayahalzyout@users.noreply.github.com>
2022-11-21 20:25:26 +0200

    Add files via upload

---
+++ 21-11/21-11/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {////task1
+            //double mydouble = 10.5;
+            //int myint = (int)mydouble;
+            //Console.WriteLine(myint);
+            //Console.ReadLine();
+
+            //////task2
+            //int ayah = Convert.ToInt32(Console.ReadLine());
+
+            //if (ayah < 0)
+            //{
+            //    Console.WriteLine("-");
+
+
+
+
+            //}
+
+            //else Console.WriteLine("+");
+
+
+
+
+            //int fn = Convert.ToInt32(Console.ReadLine());
+            //int sn = Convert.ToInt32(Console.ReadLine());
+            //int thn = Convert.ToInt32(Console.ReadLine());
+
+            //int[] btool = { fn, sn, thn };
+            //Array.Sort(btool);
+            //Console.WriteLine(btool[2] + " , " + btool[1] + "," + btool[0]);
+
+
+            /////task4
+            //int f = Convert.ToInt32(Console.ReadLine());
+            //int s = Convert.ToInt32(Console.ReadLine());
+            //int t = Convert.ToInt32(Console.ReadLine());
+            //int fi = Convert.ToInt32(Console.ReadLine());
+            //int si = Convert.ToInt32(Console.ReadLine());
+            //int[] l = { f, s, t,fi,si };
+            //Array.Sort(l);
+            //Console.WriteLine(l[4]);
+
+
+            ///task5
+            Console.WriteLine("Write the distance from your house to the company");
+            double ayah = Convert.ToInt32(Console.ReadLine());
+            double alaa = ayah * 0.621371;
+            Console.WriteLine(alaa);
+
+
+            ///task6
+            //Console.WriteLine("Please write the time ");
+            //int h = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please type the minutes");
+            //int g = Convert.ToInt32(Console.ReadLine());
+            //int c = h * 60;
+            //int end = c + g;
+            //Console.WriteLine(end);
+
+            ///task7
+            //Console.WriteLine("Please type the minutes");
+            //int go = Convert.ToInt32(Console.ReadLine());
+            //float ca = go / 60;
+            //int a = go % 60;
+            //Console.WriteLine(ca + "hours" + a + "minute");
+            
+        }
+    }
+}
