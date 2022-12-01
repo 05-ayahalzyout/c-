cf6697e86f7927f7216aab9312338c983e85aaac
05-ayahalzyout <115452706+05-ayahalzyout@users.noreply.github.com>
2022-12-01 09:20:33 +0200

    Add files via upload

---
+++ 30-11/task1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="task1.aspx.cs" Inherits="_30_11.task1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="x" runat="server" OnTextChanged="x_TextChanged"></asp:TextBox>
+             <asp:TextBox ID="y" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Combination" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Subtraction" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="flailing" OnClick="Button3_Click"/>
+            <asp:Button ID="Button4" runat="server" Text="Division" OnClick="Button4_Click" />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Resalt"></asp:Label>
+
+            <asp:Label ID="Label1" runat="server" Text=" "></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 30-11/task1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _30_11
+{
+    public partial class task1 : System.Web.UI.Page
+    {
+
+        class math { 
+        public int sum(int x,int y)
+            {
+                return x + y;
+
+            }
+            public int m(int x, int y)
+            {
+                return x - y;
+
+            }
+            public int um(int x, int y)
+            {
+                return x * y;
+
+            }
+            public int su(int x, int y)
+            {
+                return x / y;
+
+            }
+
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            math sum1= new math();
+          int result=  sum1.sum(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
+            Label1.Text = Convert.ToString( result);
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            math sum2 = new math();
+            int result = sum2.m(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
+            Label1.Text = Convert.ToString(result);
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            math sum1 = new math();
+            int result = sum1.um(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
+            Label1.Text = Convert.ToString(result);
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            math sum1 = new math();
+            int result = sum1.su(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
+            Label1.Text = Convert.ToString(result);
+        }
+
+        protected void x_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
